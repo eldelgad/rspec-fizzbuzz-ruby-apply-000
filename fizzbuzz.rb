97f24c4b9ec5a2516727a35636d0e06d886ae961
Elias Delgado <evdelgado99@gmail.com>
2020-08-14 05:51:18 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,17 +2,18 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz (int)
-  
+
   if int % 3 == 0 && int % 5 == 0
      "FizzBuzz"
-  
+     
   elsif int % 3 == 0
      "Fizz"
      
   elsif int % 5 == 0
      "Buzz"
      
-  else int % 3 != 0 && int % 5 != 0
-     
+  else
+    int
+    
    end
 end
